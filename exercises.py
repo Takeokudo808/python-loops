268b548afac71457cf0798337aefa8af539f8b2a
Shinsou <takeokudo808@gmail.com>
2018-08-20 21:11:26 -1000

    Finished Question 8

--- exercises.py
+++ exercises.py
@@ -85,7 +85,12 @@ print(even_list)
 # Create a function named add_up which takes a parameter num. In the code block inside the function, create a variable named sum and assign it a number value of 0. Next, create a for loop that will iterate through a list of numbers using the range function that will be determined by the num parameter and will sum up all the numbers in the list and store it to the sum variable. Print the sum variable to see your results.
 
 #i.e a number list of 10 will have a sum total of 45
-
+def add_up(num):
+    sum = 0
+    for number in range(num):
+        sum += number
+    print(sum)
+add_up(10)
 
 #9 East Coast vs West Coast - A Hip Hop Rivalry
 #The East Coast - West Coast hip hop rivalry was a feud between artist and fans of the East Coast hip hop and West Coast hip hop scenes from the mid to last 1990s. 
